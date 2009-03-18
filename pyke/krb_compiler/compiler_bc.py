9281a284579cf2ab84885de9fbec462c9941e3d9
mtnyogi <none@none>
2009-03-18 18:55:20 +0000

    Final release 1.0 commit.
    --HG--
    extra : convert_revision : svn%3A280a1989-90ef-4448-b869-2704929e1777/trunk%40188

--- pyke/krb_compiler/compiler_bc.py
+++ pyke/krb_compiler/compiler_bc.py
@@ -4,7 +4,7 @@ from __future__ import with_statement
 import itertools
 from pyke import contexts, pattern, bc_rule
 
-version = '0.7'
+version = '1.0'
 
 def file(rule, arg_patterns, arg_context):
   engine = rule.rule_base.engine
